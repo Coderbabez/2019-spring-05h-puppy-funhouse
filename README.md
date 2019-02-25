ece50f25d8838040ed45277eea0b708924c091c7
Meghan Knoll <megknoll@gmail.com>
2019-02-25 14:01:07 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# CoderBabez
+
+##  Week Five - Javascript Event handlers
+
+### Objectives
+Review event handlers and JQuery functions. Learn to use the JQuery documentation page.
+
+# Reference:
+* https://api.jquery.com/click/
+* https://api.jquery.com/nth-child-selector/
+
+### Homework
+Follow the instructions in the comments in the script.js file.
+You SHOULD NOT modify the CSS or html files for this exercise (for this exercise only it counts as cheating).
---
+++ script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+/* CHALLENGE #1:
+    - when a user clicks on the happy button, make the background of the page yellow
+    - when a user clicks on the sad button, make the background of the page blue */
+
+
+
+/* CHALLENGE #2:
+    - when a user clicks on any of the circles, change the h1 text and the border of the image to the color of the circle*/
+
+
+
+/* CHALLENGE #3:
+    - when a user hovers on the dog image, change it to an image of your dog.
+    - when a user is no longer hovering, change it back to tonks   */
+
+
+
+/* CHALLENGE #4:
+    - when a user double clicks on CHANGE ME, change the text to "I changed"!*/
+
+
+
+/* CHALLENGE #5:
+    - when a user scrolls, add a new image of a dog of your choice to the bottom of the page.*/
+
+
+
+});
